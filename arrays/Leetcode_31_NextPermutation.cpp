24fcd0b1e8cbb72046b0f30fdcde10e0e91e0896
unknown <wangk@whu.edu.cn>
2020-11-09 22:17:36 +0800

    add 31

---
+++ arrays/Leetcode_31_NextPermutation.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/next-permutation/
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Find the largest index k such that nums[k] < nums[k + 1]. If no such index exists, just reverse nums and done.
+// Find the largest index l > k such that nums[k] < nums[l].
+// Swap nums[k] and nums[l].
+// Reverse the sub-array nums[k + 1:].
+void nextPermutation(vector<int> &nums)
+{
+    int n = nums.size(), k, l;
+    for (k = n - 2; k >= 0; k--)
+    {
+        if (nums[k] < nums[k + 1])
+        {
+            break;
+        }
+    }
+    if (k < 0)
+    {
+        reverse(nums.begin(), nums.end());
+    }
+    else
+    {
+        for (l = n - 1; l > k; l--)
+        {
+            if (nums[l] > nums[k])
+            {
+                break;
+            }
+        }
+        swap(nums[k], nums[l]);
+        reverse(nums.begin() + k + 1, nums.end());
+    }
+}
+
+int main()
+{
+    vector<int> nums = {2, 3, 6, 5, 4, 1};
+    nextPermutation(nums);
+
+}
---
+++ arrays/Leetcode_33_RotatedSorted.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target=0;
+
+}
